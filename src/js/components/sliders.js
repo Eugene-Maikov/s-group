f0d060baab301c57b01c3a3e99d51654b498b320
Евгений Майков <eugene-maikov2000@yandex.ru>
2022-09-12 18:56:19 +0300

    added sections: parners, reviews, questions

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,27 +1,84 @@
 // Подключение свайпера
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper(".news__slider", {
+const swiperNews = new Swiper(".news__slider", {
   slidesPerView: 3,
-  spaceBetween: 27,
+  spaceBetween: 30,
 
   navigation: {
     nextEl: ".news__btn-next",
     prevEl: ".news__btn-prev",
   },
+  clickable: true,
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    400: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
+  pagination: {
+    el: '.news-pagination',
   },
+
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 10,
+  //   },
+  //   400: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 10,
+  //   },
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 10,
+  //   },
+  // },
+});
+const swiperPartners = new Swiper(".partners__slider", {
+  slidesPerView: 6,
+  spaceBetween: 50,
+
+  navigation: {
+    nextEl: ".partners__btn-next",
+    prevEl: ".partners__btn-prev",
+  },
+
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 10,
+  //   },
+  //   400: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 10,
+  //   },
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 10,
+  //   },
+  // },
+});
+const swiperReviews = new Swiper(".reviews__slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".reviews__btn-next",
+    prevEl: ".reviews__btn-prev",
+  },
+
+  pagination: {
+    el: '.reviews-pagination',
+  },
+
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 10,
+  //   },
+  //   400: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 10,
+  //   },
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 10,
+  //   },
+  // },
 });

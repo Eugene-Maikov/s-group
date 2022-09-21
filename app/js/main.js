ae602f3b4c5168ecb11579b92a68b51a0b23a78c
Евгений Майков <eugene-maikov2000@yandex.ru>
2022-09-21 19:02:27 +0300

    Adedd map

--- app/js/main.js
+++ app/js/main.js
@@ -161,19 +161,52 @@ contactItems.forEach((item, i, a) => {
   \***************************************/
 /***/ (() => {
 
-const contactMap = document.getElementById("map_canvas");
-
-function initialize() {
-  let myLatlng = new google.maps.LatLng(-34.397, 150.644);
-  let myOptions = {
-    zoom: 8,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  let map = new google.maps.Map(contactMap, myOptions);
-}
-
-initialize();
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+    center: [55.751574, 37.573856],
+    zoom: 9
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+      // Создаём макет содержимого.
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'),
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    hintContent: 'Собственный значок метки',
+    balloonContent: 'Это красивая метка'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: '../img/location.svg',
+    // Размеры метки.
+    iconImageSize: [56, 80],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-5, -38]
+  }),
+      myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+    hintContent: 'Собственный значок метки с контентом',
+    balloonContent: 'А эта — новогодняя',
+    iconContent: 'ул. Костина, 6к1'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#imageWithContent',
+    // Своё изображение иконки метки.
+    iconImageHref: '../img/location.svg',
+    // Размеры метки.
+    iconImageSize: [56, 80],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-24, -24],
+    // Смещение слоя с содержимым относительно слоя с картинкой.
+    iconContentOffset: [15, 15],
+    // Макет содержимого.
+    iconContentLayout: MyIconContentLayout
+  });
+  myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+});
 
 /***/ }),
 
